7345779c5ced8703e3d65fa9ee47cb1dee4fe293
Julcess Marie <julcessmariemercado@gmail.com>
2022-08-11 15:42:47 +0800

    update login

--- modules/login/functions/logout.php
+++ modules/login/functions/logout.php
@@ -6,5 +6,5 @@ require($_SERVER['DOCUMENT_ROOT'].'/includes/config.php');
 session_start();
 
 $_SESSION['sys_login_suc'] = renderLang($login_logout_successfully);
-header('location: /admin-login');
+header('location: /login');
 ?>
