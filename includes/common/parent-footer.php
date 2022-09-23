0bb3b3fa00485fbd8053957b1395127c86b2d613
Kim De Leon <71622737+kimdl@users.noreply.github.com>
2022-09-23 08:45:30 +0800

    footer fix parent

--- includes/common/parent-footer.php
+++ includes/common/parent-footer.php
@@ -1,23 +1,13 @@
 <script src="assets/js/jquery.js"></script>
-<!-- <div class="footer">
+<div class="footer">
     <div class="footerBlock">
         <div class="footerLeft">
             <p><strong>DX Info Hub</strong>
                 powered by DX Offshore Team</p>
-            <p>©transcosmos Asia Philippines</p>
+            <p>©transcosmos Asia Philippines <?php echo date("Y"); ?></p>
         </div>
         <div class="footerRight">
             <img src="/assets/images/dxInfo-logo-img02.svg" >
         </div>
     </div>
-</div> -->
-<footer class="footer">
-    <div class="footerBlock">
-        <strong><a href="https://adminlte.io">DX Info Hub</a> powered by DX Offshore Team.</strong>
-        
-        <div class="float-right d-none d-sm-inline-block">
-            &copy; transcosmos Asia Philippines  <?php echo date("Y"); ?>
-        </div>
-    </div>
-    
-</footer>
+</div>
