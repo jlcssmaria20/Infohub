eda154811aaf6f4b4ab5f315b0c1579f59003f4f
Kim De Leon <71622737+kimdl@users.noreply.github.com>
2022-10-13 13:25:36 +0800

    search on documents

--- pages/documents/o-index.php
+++ pages/documents/o-index.php
@@ -16,6 +16,8 @@ require($_SERVER['DOCUMENT_ROOT'].'/includes/config.php');
         <title><?php echo $dx."Documents and Links"; ?></title>
         <link href="/assets/css/documents.css" rel="stylesheet" />
         <?php require($_SERVER['DOCUMENT_ROOT'].'/includes/common/links.php');  ?>
+
+        <style></style>
     </head>
     <body id="documents">
         <div class="container">
@@ -25,21 +27,26 @@ require($_SERVER['DOCUMENT_ROOT'].'/includes/config.php');
 
             <section class="main-area col-s-9 d-column mb-4 ml-5">
                 <div class="announcement mb-4">
-                    <h2 class="mb-3">
-                        <span  style="color: var(--black);">Documents and Quick Links</span>
-                    </h2>
-                    <div class="announcement-row">
-
-                        <ul class="list-inline mb-4">
+                    <div class="row">
+                        <div class="col-lg-9">
+                            <h2 class="mb-3">
+                                Documents and Quick Links
+                            </h2>
+                        </div>
+                        <div class="col-lg-3">
+                            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Enter Document Name or Description" class="form-control mr-2 px-2">
+                        </div>
+                    </div>
+                  
+                    <div class="announcement-row">                      
+                        <ul class="list-inline mb-4" id="myUL">
                             <?php
                                 $data_count = 0;
                                 $sql = $pdo->prepare("SELECT * FROM documents WHERE `document_status` = 0 ORDER BY id ASC");
                                 $sql->execute();
                                 while($data = $sql->fetch(PDO::FETCH_ASSOC)) {
-
                                 $data_count++;
                                 $document_id = encryptID($data['id']);
-                                
                                 $count =  $pdo->prepare("SELECT * FROM `files` WHERE `document_id` = ".$data['id']);
                                 $count->execute();
                                 $total_data_count = $count->rowCount();
@@ -57,11 +64,31 @@ require($_SERVER['DOCUMENT_ROOT'].'/includes/config.php');
                         </ul>
                     </div>
                 </div>
+                
             </section>       
             </div>
         </div>
         <?php require($_SERVER['DOCUMENT_ROOT'].'/includes/common/parent-footer.php');  ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script src="assets/modal/js/lightslider.js"></script> 
+        <script>
+                        
+            function myFunction() {
+                var input, filter, ul, li, a, i, txtValue;
+                input = document.getElementById("myInput");
+                filter = input.value.toUpperCase();
+                ul = document.getElementById("myUL");
+                a = ul.getElementsByTagName("a");
+                for (i = 0; i < a.length; i++) {
+                    li = a[i].getElementsByTagName("li")[0];
+                    txtValue = li.textContent || li.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        a[i].style.display = "";
+                    } else {
+                        a[i].style.display = "none";
+                    }
+                }
+            }   
+        </script>
     </body>
 </html>
