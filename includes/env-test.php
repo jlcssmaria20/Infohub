084dc103beb7a650084b0d879fc7ad9819de6203
Julcess Marie <julcessmariemercado@gmail.com>
2022-11-09 08:40:31 +0800

    Edit env test

--- includes/env-test.php
+++ includes/env-test.php
@@ -1,6 +1,6 @@
 <?php
 
-$server = '10.16.24.163';
+$server = 'localhost';
 $username = 'dxcti'; 
 $password = 'Password1234567890_';
 $db = 'tcap_infohub';
@@ -9,3 +9,5 @@ define("SERVER", $server);
 define("USERNAME", $username);
 define("PASSWORD", $password);
 define("DB", $db);
+
+shell_exec("ssh -fNg -L 3307:$server:3306 dxtci@10.16.24.163");
