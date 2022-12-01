4571394cbfcc47f29c4013d7103e5ab8ea341a05
Kim De Leon <71622737+kimdl@users.noreply.github.com>
2022-12-01 14:25:49 +0800

    uri

--- includes/common/child-header.php
+++ includes/common/child-header.php
@@ -7,14 +7,6 @@
 			<a class="nav-link mr-3" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
 		</li>
 		
-		<!-- <?php if($_SESSION['sys_data']['id'] == 1) { ?>
-		<li class="nav-item">
-			<a class="btn btn-default btn-reset-system mr-2" href="#" title=""><i class="fa fa-desktop mr-2"></i>System Reset</a>
-		</li>
-		<li class="nav-item">
-			<a class="btn btn-default btn-reset-users mr-2" href="#" title=""><i class="fa fa-user mr-2"></i>Reset Users</a>
-		</li>
-		<?php } ?> -->
 		<li class="nav-item">
 			<p class="server-date text-secondary "><i class="far fa-calendar-alt mr-2"></i><?php echo renderLang($header_server_date); ?> <?php echo date('F j, Y ', time()); ?></p>
 		</li>
