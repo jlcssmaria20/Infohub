56833d4d976253c5495b7ff294832aea3adc0a9c
Julcess Marie <julcessmariemercado@gmail.com>
2023-01-11 14:45:57 +0800

    delete confirmation not working in roles

--- includes/functions.php
+++ includes/functions.php
@@ -34,7 +34,7 @@ function renderConfirmDelete($err_code,$session_name,$variable_name) {
 	switch($err_code) {
 		case 0:
 			$_SESSION[$session_name] = renderLang($GLOBALS[$variable_name]);
-			echo $session_name;
+			// echo $session_name;
 			if(($session_name == 'sys_users_suc') || ($session_name == 'sys_roles_suc')){
 				echo '1,success';
 			}
